5bf5911a4a12b8d17c27a89d980c0e5d8ddddd18
Mahad-138 <mmahadshahzad@gmail.com>
2026-05-20 17:57:08 +0100

    ParticlesEffect
    Adding particle effects to the jetpack

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -5,12 +5,14 @@ public class Car : MonoBehaviour
 {
     [SerializeField] Rigidbody2D rb;
     [SerializeField] float upspeed;
+    [SerializeField] ParticleSystem ps;
+    private ParticleSystem.EmissionModule em;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        em = ps.emission;
     }
 
     // Update is called once per frame
@@ -19,6 +21,11 @@ public class Car : MonoBehaviour
         if (Keyboard.current.spaceKey.isPressed)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, upspeed);
+            em.enabled = true;
+        }
+        else
+        {
+            em.enabled = false;
         }
     }
 }
